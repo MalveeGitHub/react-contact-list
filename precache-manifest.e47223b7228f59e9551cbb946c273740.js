bd7f9fca5a3171f03c0b1a17ae3f647dbb2cfbe9
alvee <you@example.com>
2018-10-12 06:14:29 -0400

    Updates

---
+++ precache-manifest.e47223b7228f59e9551cbb946c273740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a047d8e7e57bcd294799",
+    "url": "/react-contact-list/static/js/runtime~main.a047d8e7.js"
+  },
+  {
+    "revision": "af0ba7b3fc6b8e0655e4",
+    "url": "/react-contact-list/static/js/main.af0ba7b3.chunk.js"
+  },
+  {
+    "revision": "835a581b597df201dba9",
+    "url": "/react-contact-list/static/js/1.835a581b.chunk.js"
+  },
+  {
+    "revision": "af0ba7b3fc6b8e0655e4",
+    "url": "/react-contact-list/static/css/main.474e6b6e.chunk.css"
+  },
+  {
+    "revision": "835a581b597df201dba9",
+    "url": "/react-contact-list/static/css/1.0d1cd484.chunk.css"
+  },
+  {
+    "revision": "ecb8011b33fe0b9e18e44b17a73d1839",
+    "url": "/react-contact-list/index.html"
+  }
+];
